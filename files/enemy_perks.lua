81b0ba214375ee54d46ac67347e56c4ef293ab9d
UserK <111083281+FatalUserK@users.noreply.github.com>
2026-05-05 01:14:22 +0100

    working on perk picker

--- files/enemy_perks.lua
+++ files/enemy_perks.lua
@@ -2,26 +2,64 @@ dofile_once("mods/divine_right/files/utilities.lua")
 
 local perks = dofile_once("mods/divine_right/files/enemy_perks_list.lua")
 local ng_plus_iter = tonumber(SessionNumbersGetValue("NEW_GAME_PLUS_COUNT")) or 0
-local dr_comp
+local drh
 
 function EnemyApplyPerk(entity_id, x, y, perk)
 	if perk.func then
-		perk.func(entity_id)
+		perk:func(entity_id)
 	end
 end
 
-local function PickRandomPerk()
+local function PickRandomPerk(perk_list, additional_context)
+	local context = {
+		int_possessed_perks = 0, --the number of perks the target already possesses before adding a new one
+		current_perks = {},
+		perks_to_add = 1, --presume only one perk is being added if additional_context doesn't override
+	}
 
+	if additional_context then
+		for key, value in pairs(additional_context) do
+			context[key] = value
+		end
+	end
+
+
+	local valid_perks = {}
+	for _, entry in ipairs(perk_list) do
+		if (entry.condition and not entry:condition(context)) --if condition has failed (runs first for pre-processing purposes)
+			or (context.current_perks[entry.id] and context.current_perks[entry.id] >= entry.max_stacks) --if current stacks is equal to or more than max stacks
+		then goto continue end
+		valid_perks[#valid_perks+1] = entry
+		::continue::
+	end
+
+	if #valid_perks == 0 then print("NO PERKS MEET CONDITIONS") return nil,context end
+
+	local total_weight = 0
+	for _, entry in ipairs(valid_perks) do
+		total_weight = total_weight + entry.weight
+	end
+
+	local rnd = Randomf(0, total_weight)
+	for _, entry in ipairs(valid_perks) do
+		if rnd <= entry.weight then
+			return entry,context
+		else rnd = rnd - entry.weight end
+	end
+	return valid_perks[#valid_perks],context --Randomf has OOB issue
 end
+
 
 function EnemyGrantRandomPerks(entity_id, x, y)
 	SetRandomSeed(entity_id - y, x)
 
+	--50% chance to add 1 perk per NG+
 	local perks_to_grant = 1
 	for i = 1, ng_plus_iter do
 		if Random() < .5 then perks_to_grant = perks_to_grant + 1 end
 	end
 
+	--recursively roll a 30% chance
 	local function add_perk(n)
 		if Random() < .3 then
 			return add_perk(n + 1)
@@ -32,11 +70,14 @@ function EnemyGrantRandomPerks(entity_id, x, y)
 	perks_to_grant = perks_to_grant + add_perk(0)
 
 	if perks_to_grant == 0 then return 0 end
+	local context = {}
 	for i = 1, perks_to_grant do
-		--EnemyApplyPerk(RandomFromTableConditional(perks))
+		local new_perk,new_context = PickRandomPerk(perks, context)
+		context = new_context
+		EnemyApplyPerk(new_perk)
 	end
 
-	EnemyApplyPerk(entity_id, x, y, perks[#perks])
+	--EnemyApplyPerk(entity_id, x, y, perks[#perks])
 end
 
 
@@ -45,13 +86,16 @@ local entity_id = GetUpdatedEntityID()
 local x,y = EntityGetTransform(entity_id)
 
 function init()
-	for index, varcomp in ipairs(EntityGetComponent(entity_id, "VariableStorageComponent") or {}) do
-		if ComponentGetValue2(varcomp, "name") == "divine_rights" then print(entity_id) return end
+	for index, child in ipairs(EntityGetAllChildren(entity_id) or {}) do
+		if EntityGetName(child) == "divine_rights_handler" then
+			drh = child
+		end
 	end
 
-	dr_comp = EntityAddComponent2(entity_id, "VariableStorageComponent", {
-		name = "divine_rights"
-	})
+	if drh == nil then
+		drh = EntityLoad("mods/divine_right/files/divine_rights_handler.xml")
+		EntityAddChild(entity_id, drh)
+	end
 
 	EnemyGrantRandomPerks(entity_id, x, y)
 end
--- files/enemy_perks_list.lua
+++ files/enemy_perks_list.lua
@@ -5,11 +5,15 @@
 ---@field icon string Path to 12x12 icon used in-world.
 ---@field weight number? Default 10.
 ---@field progress_icon string? Path to 12x12 icon used in progress menu. If blank, mod will automatically add a red border to the perk.
+---@field max_stacks number? Max number of times an enemy can roll this perk, infinite if nil.
 ---@field condition bool|function? If set, condition must pass in order for the perk to be chosen. Function can be used here to additionally modify the perk itself, including probability
 ---@field game_effect string? If set, will add a `GameEffectComponent` to the perk entity with `game_effect` as its ID.
 ---@field load_entity string? If set, will load target entity as the base for the perk entity as a child to the perk owner.
 ---@field func function? The function that's run when the perk is added.
 ---@field func_remove function? The function that's run when the perk is removed.
+---
+---Generic optional vals used for some stuff
+---@field blood_perk bool?
 
 
 ---@type enemy_perk[]
@@ -20,6 +24,7 @@ local perks = {
 		description = "",
 		icon = "",
 		progress_icon = nil,
+		max_stacks = 3,
 		condition = nil,
 		game_effect = nil, --load game effect
 		load_entity = nil,
@@ -33,13 +38,14 @@ local perks = {
 		icon = "",
 		ui_name = "",
 		description = "",
-		progress_icon = "", --
-		func = function(entity_id, copies, x, y)
+		progress_icon = "",
+		max_stacks = 1,
+		func = function(self, entity_id, copies, x, y)
 			EntityAddComponent2(entity_id, "LuaComponent", {
 				script_damage_received = "mods/divine_right/files/scripts/swapper.lua"
 			})
 		end,
-		func_remove = function(entity_id)
+		func_remove = function(self, entity_id) --func remove may not require `self`, will retract this param if i end up never using it
 			for _,comp in ipairs(EntityGetComponent(entity_id, "LuaComponent") or {}) do
 				if ComponentGetValue2(comp, "script_damage_received") == "mods/divine_right/files/scripts/swapper.lua" then
 					EntityRemoveComponent(entity_id, comp)
@@ -55,5 +61,13 @@ local perks = {
 do_mod_appends("mods/divine_right/files/enemy_perks_list.lua")
 for index, perk in ipairs(perks) do
 	perk.weight = perk.weight or 10
+	if not ModDoesFileExist(perk.icon or "") then
+		perk.icon = "mods/divine_right/files/perk_bg.png"
+		if not ModDoesFileExist(perk.progress_icon or "") then
+			perk.icon = "mods/divine_right/files/perk_progress_bg.png"
+		end
+	elseif not ModDoesFileExist(perk.progress_icon or "") then
+		
+	end
 end
 return perks
